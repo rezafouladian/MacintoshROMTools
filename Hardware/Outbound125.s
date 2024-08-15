6f170470936bb57306fc9bf284d801f43d2b1b45
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2024-08-14 22:52:46 -0700

    Add display RAM and additional config bits

--- Hardware/Outbound125.s
+++ Hardware/Outbound125.s
@@ -1,5 +1,6 @@
 BaseOfROM       EQU         $400000
 PtchROMBase     EQU         $F80000
+OutboundDisp    EQU         $700000                 ; Outbound 32KB SRAM/Interal Display
 
 PtchTblBase     EQU         $707D04                 ; Pointer to current patch source
 ExpectedPC      EQU         $707D0A                 ; Low word of PC from exception to check for patching
@@ -8,3 +9,6 @@ PatchTblPtr     EQU         $707D18                 ; Pointer to current locatio
 
 OutboundCfg     EQU         $707D08
 IsMacSEROM      EQU         0                       ; Bit 0 = Mac SE ROM
+CfgBit1         EQU         1
+CfgBit3         EQU         3
+CfgBit7         EQU         7
