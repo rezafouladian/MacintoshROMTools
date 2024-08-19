62e007255267405e7da970ce798dacdc1bbe9d0c
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2024-08-19 08:28:04 -0700

    Update Outbound125.s

--- Hardware/Outbound125.s
+++ Hardware/Outbound125.s
@@ -1,20 +1,21 @@
 BaseOfROM       EQU         $400000
 PtchROMBase     EQU         $F80000
 OutboundDisp    EQU         $700000                 ; Outbound 32KB SRAM/Internal Display
-OutboundVIA     EQU         $E7E1FE
+OutboundVIA     EQU         $E7E1FE                 ; Unconfirmed
 
 PtchTblBase     EQU         $707D04                 ; Pointer to current patch source
 ExpectedPC      EQU         $707D0A                 ; Low word of PC from exception to check for patching
 PatchOffset     EQU         $707D0C                 ; Offset from PatchTableBase to code to run
 PatchTblPtr     EQU         $707D18                 ; Pointer to current location in patch table
 
-OutboundCfg     EQU         $707D08
+OutboundCfg     EQU         $707D08                 ; Space for marking hardware config of some sort
 IsMacSEROM      EQU         0                       ; Bit 0 = Mac SE ROM
 CfgBit1         EQU         1
 CfgBit2         EQU         2
 CfgBit3         EQU         3
 CfgBit5         EQU         5
 CfgBit7         EQU         7
+OutboundCfg2    EQU         $707D09                 ; A second config space?
 
 
 ; Host SE or Plus Hardware
