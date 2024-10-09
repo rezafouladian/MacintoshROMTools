f41b6e598d9146a7a3672056c394c831661cbd85
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2024-10-09 02:39:44 -0700

    Add SE VIA equates

--- Hardware/SE.s
+++ Hardware/SE.s
@@ -1,3 +1,10 @@
 BaseOfROM       EQU         $400000
 
 HeapStart       EQU         $1600
+
+VBase           EQU         $EFE1FE
+
+vBufB           EQU         $0
+vDIRB           EQU         $400
+vDIRA           EQU         $600
+vBufA           EQU         $1E00
