40ab55e8163dd4664bdbfbb034fc833ba428d8f5
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2025-02-22 00:38:10 -0800

    Add bufWorldSize

--- Hardware/PowerBook170.s
+++ Hardware/PowerBook170.s
@@ -1,2 +1,3 @@
                 INCLUDE     'VIA.s'                 ; Include VIA offsets
                 INCLUDE     'SCSI.s'                ; Include SCSI offsets
+bufWorldSize    EQU         $800000
