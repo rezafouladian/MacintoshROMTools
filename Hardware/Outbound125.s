f1dc9a0f6d4bec38d64b18ac05e9ba5f75b7cf57
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2026-01-08 09:45:45 -0800

    Update config bit labels

--- Hardware/Outbound125.s
+++ Hardware/Outbound125.s
@@ -13,8 +13,8 @@ PatchTblPtr     EQU         $707D18                 ; Pointer to current locatio
 OutboundCfg     EQU         $707D08                 ; Space for marking hardware config of some sort
 IsMacSEROM      EQU         0                       ; Bit 0 = Mac SE ROM
 CfgBit1         EQU         1
-CfgBit2         EQU         2                       ; SCSI related?
-CfgBit3         EQU         3                       ; Possibly for marking if a host system is present
+SCSIPresent     EQU         2                       ; SCSI related?
+HostMac         EQU         3                       ; Possibly for marking if a host system is present
 CfgBit4         EQU         4
 CfgBit5         EQU         5
 CfgBit6         EQU         6
